355f84d1226f11cdc46bc035ba9c86b5028c916f
idamtk <idamtkrarup@gmail.com>
2024-05-30 14:01:02 +0200

    Translated categories to Danish

--- app/app draft.R
+++ app/app draft.R
@@ -6,11 +6,86 @@
 #
 #    http://shiny.rstudio.com/
 #
+
+wheels_data_aar <- wheels_data_aar  %>%
+  mutate(cat_da = case_when(
+    amenity == "fuel" ~ "Benzintank",
+    amenity == "post_office" ~ "Posthus",
+    amenity == "cinema" ~ "Biograf",
+    amenity == "fast_food" ~ "Restaurant",
+    amenity == "nightclub" ~ "Natklub",
+    amenity == "bar" ~ "Bar",
+    amenity == "cafe" ~ "Café",
+    amenity == "pharmacy" ~ "Apotek",
+    amenity == "theatre" ~ "teater",
+    amenity == "clinic" ~ "Læge",
+    amenity == "school" ~ "Skole",
+    amenity == "restaurant" ~ "Restaurant",
+    amenity == "library" ~ "Bibliotek",
+    amenity == "bank" ~ "Bank",
+    amenity == "pub" ~ "Bar",
+    amenity == "hookah_lounge" ~ "Vandpibecafé",
+    amenity == "social_facility" ~ "Sociale ydelser",
+    amenity == "ice_cream" ~ "Iskiosk",
+    amenity == "bureau_de_change" ~ "Udvekslingsbureau",
+    amenity == "doctors" ~ "Læge"
+    ))
+
 wheels_data_cph <- wheels_data_cph  %>%
   mutate(label_da = case_when(
     wheelchair == "yes" ~ "Tilgængelig",
     wheelchair == "limited" ~ "Begrænset tilgængelighed",
-    wheelchair == "no" ~ "Utilgænglig"
+    wheelchair == "no" ~ "Utilgængelig"
+  ))
+wheels_data_cph <- wheels_data_cph  %>%
+  mutate(cat_da = case_when(
+    amenity == "resturant" ~ "Restaurant",
+    amenity == "fuel" ~ "Benzintank",
+    amenity == "library" ~ "Bibliotek",
+    amenity == "cinema" ~ "Biograf",
+    amenity == "theatre" ~ "Teater",
+    amenity == "cafe" ~ "Café",
+    amenity == "pub" ~ "Bar",
+    amenity == "fast_food" ~ "Restaurant",
+    amenity == "bank" ~ "Bank",
+    amenity == "car_rental" ~ "Biludlejning",
+    amenity == "bureau_de_change" ~ "Udvekslingsbureau",
+    amenity == "pharmacy" ~ "Apotek",
+    amenity == "nightclub" ~ "Natklub",
+    amenity == "ferry_terminal" ~ "Færge",
+    amenity == "school" ~ "Skole",
+    amenity == "parking_entrance" ~ "Parkering",
+    amenity == "bar" ~ "Bar",
+    amenity == "bicycle_rental" ~ "Cykelludlejning",
+    amenity == "music_venue" ~ "Koncerthus",
+    amenity == "dentist" ~ "Tandlæge",
+    amenity == "police" ~ "Politistation",
+    amenity == "toilets" ~ "Toilet",
+    amenity == "doctors" ~ "Læge",
+    amenity == "recycling" ~ "Genbrugsstation",
+    amenity == "veterinary" ~ "Dyrlæge",
+    amenity == "parcel_locker" ~ "Pakkeboks",
+    amenity == "post_office" ~ "Posthus",
+    amenity == "ice_cream" ~ "Iskiosk",
+    amenity == "bus_station" ~ "Busstation",
+    amenity == "community_centre" ~ "Kulturhus",
+    amenity == "car_wash" ~ "Bilvask",
+    amenity == "driving_school" ~ "Køreskole",
+    amenity == "clinic" ~ "Læge",
+    amenity == "photo" ~ "Fotografering",
+    amenity == "stripclub" ~ "Stripklub",
+    amenity == "dancing_school" ~ "Danse undervisning",
+    amenity == "brothel" ~ "Bordel",
+    amenity == "coworking_space" ~ "Kontorfællesskab",
+    amenity == "social_facility" ~ "Sociale initiativer",
+    amenity == "events_venue" ~ "Mødested",
+    amenity == "studio" ~ "Studio", ## this category is heterogeneous
+    amenity == "shelter" ~ "Shelter",
+    amenity == "language_school" ~ "Sprogskole",
+    amenity == "place_of_worship" ~ "Trossamfund",
+    amenity == "casino" ~ "Kasino",
+    amenity == "kindergarten" ~ "Børnehave",
+    amenity == "hookah_lounge" ~ "Vandpibecafé",
   ))
 setwd("~/OneDrive/Skrivebord/Sjette semester/Spatial Analytics/exam/accessibleaarhus/app")
 library(stringr)
@@ -20,12 +95,13 @@ wheels_data_cph <- readr::read_rds("../data_processing/cph_map_data.rds")
 wheels_data_aar <- readr::read_rds("../data_processing/aarhus_map_data.rds")
 metrics<-read.csv("../analysis/metrics.csv",fileEncoding = "WINDOWS-1252")
 wheel <- list(cph=wheels_data_cph,aar=wheels_data_aar)
-cat_aar <- unique(wheels_data_aar$amenity)
+aar_yes<- wheels_data_aar%>% filter(wheelchair=="yes")
+cat_aar <- unique(wheels_data_aar$cat_da)
 cat_aar <- cat_aar[-1]
-cat_cph <- unique(wheels_data_cph$amenity)
-cat_cph[1] <- "All"
-categories<-sort(union(cat_aar,cat_cph))
-amenities <- c("All", "parking", "restaurant")
+cat_cph <- unique(wheels_data_cph$cat_da)
+cat_cph[1] <- "Vælg"
+categories<-union(cat_cph,cat_aar)
+amenities <- c("Vælg", "parking", "restaurant")
 token <- "pk.eyJ1IjoiaWRhbXRrIiwiYSI6ImNsdGUycjB3dTBhbmkyaXJ0bnh4dGt6MGwifQ.rfH9mhRsZwCVuTXz34PiSA"
 library(bslib)
 library(purrr)
@@ -139,12 +215,12 @@ server <- function(input, output, session) {
   
     
   output$map <- renderLeaflet({
-      if (input$amenity=="All"){
+      if (input$amenity=="Vælg"){
         wheels_data <- wheels$data
       } 
       else {
         wheels_data <- wheels$data
-        wheels_data_filtered <- dplyr::filter(wheels_data, wheelchair=="yes" & amenity==input$amenity)
+        wheels_data_filtered <- dplyr::filter(wheels_data, wheelchair=="yes" & cat_da==input$amenity)
         if (nrow(wheels_data_filtered)==0){
           shinyalert("No data available!", "There are no accessible locations in this category", type = "error")
           
--- data_processing/test.r
+++ data_processing/test.r
@@ -1,39 +1,9 @@
-library(httr)
+library(tidyverse)
+url <- "https://api.dataforsyningen.dk/postnumre?format=geojson"
+geofile<- tempfile()
+download.file(url,geofile)
 library(sf)
-library(xml2)
-
-# Define the URL with your token
-url <- "https://api.dataforsyningen.dk/DAGI_10MULTIGEOM_GMLSFP_DAF?service=WFS&request=GetCapabilities&token=8fdc7553a1e8adc188ecd830dfb7f7c4"
-
-# Make a GET request
-response <- GET(url, query=list(request="POSTNUMMERINDDELING"))
-
-# Check if the request was successful
-if (status_code(response) == 200) {
-  # Parse the JSON response
-  data <- content(response, "parsed")
-  
-  # Now you can work with the data
-  print(data)
-} else {
-  # Print error message if request failed
-  print("Error: Failed to retrieve data")
-}
-library(xml2)
-
-# Parse the XML document
-doc <- xml2::read_xml(response)
-
-# Extract the data you need
-# For example, to extract the Title from ServiceIdentification
-all <- xml2::xml_text(xml2::xml_find_fir(doc))
-
-# Print the extracted data
-print(all)
-
-library(XML)
-
-data <- xmlParse(response)
-
-xml_data <- xmlToList(data)
-print(xml_data)
+geodata<-st_read(geofile)
+aar_c<-geodata%>%dplyr::filter(nr==8000)
+aar_c_4326 <- aar_c%>% st_transform(4326)
+saveRDS(aar_c_4326,"aarhus_c.rds")
