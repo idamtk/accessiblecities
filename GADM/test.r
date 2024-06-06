536d23578dad7856cd932336ef03a5465c6911f0
idamtk <136336083+idamtk@users.noreply.github.com>
2024-06-06 11:31:16 +0200

    Delete GADM directory

--- GADM/test.r
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-url <- "https://api.dataforsyningen.dk/postnumre?format=geojson"
-geofile<- tempfile()
-download.file(url,geofile)
-library(sf)
-geodata<-st_read(geofile)
-aar_c<-geodata%>%dplyr::filter(nr==8000)
-aar_c_4326 <- aar_c%>% st_transform(4326)
-saveRDS(aar_c_4326,"aarhus_c.rds")
